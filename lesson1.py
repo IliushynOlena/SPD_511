44db676364634c014ba5c549b9ffe853e81e6b2f
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2025-12-06 16:01:43 +0200

    first commit

---
+++ lesson1.py
@@ -0,0 +1,27 @@
+#Ctrl + F5 - start program
+# \n  -  new line
+# \t  -  tabulation (8 spaces)
+# \"   - "Python"
+# \\   - \Python\
+print("I\n\tlove\n\t\t\"Python\"")
+print('I\n\tlove\n\t\t"Python"')
+print("I\n\tlove\n\t\t\\Python\\")
+
+print("Hello, World!")
+print("I")
+print("love")
+print("Python")
+print('Hello, World!')
+print("Python")
+print("Python")
+print("I love Python")
+print("I love Python")
+#Alt + Shift + arrow down   - Dublicate row
+#Alt + arrow down   - move row
+#Ctrl + D + select all same elements
+
+print("P","y","t","h","o","n", sep='\n')
+
+print("red","green","blue","white",sep='*', end=" ")
+
+print("Olga","Inna","Ivan", sep='_')
